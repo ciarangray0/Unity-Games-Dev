0133f34fa3fd18870a3f60b276170e13340dcf10
ciarangray0 <ciarangray0@gmail.com>
2024-09-16 16:47:36 +0100

    added mars' moons and camera movement

--- mars/Assets/GameManagerScript.cs
+++ mars/Assets/GameManagerScript.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class GameManagerScript : MonoBehaviour
 {
     public GameObject mars;
+    public GameObject deimos;
+    public GameObject phobos;
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.transform.position = new Vector3(0f, 0f, -100f);
+        Camera.main.transform.position = new Vector3(0f, 0f, -150f);
         Camera.main.transform.LookAt(mars.transform);
 
         mars.GetComponent<Rigidbody>().AddTorque(new Vector3(0f, 10f, 0f));
@@ -17,6 +19,21 @@ public class GameManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        phobos.transform.RotateAround(mars.transform.position, new Vector3(0f, 1f, 0f), 10*Time.deltaTime);
+        deimos.transform.RotateAround(mars.transform.position, new Vector3(0f, 1f, 0f), 10*Time.deltaTime);
+
+        //Camera.main.transform.LookAt(mars.transform);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) 
+        Camera.main.transform.RotateAround(mars.transform.position, new Vector3(0f, 1f, 0f), 10*Time.deltaTime);
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        Camera.main.transform.RotateAround(mars.transform.position, new Vector3(0f, -1f, 0f), 10*Time.deltaTime);
+
+        else if (Input.GetKey(KeyCode.UpArrow))
+        Camera.main.transform.RotateAround(mars.transform.position, new Vector3(1f, 0f, 0f), 10 * Time.deltaTime);
+
+        else if (Input.GetKey(KeyCode.DownArrow))
+        Camera.main.transform.RotateAround(mars.transform.position, new Vector3(-1f, 0f, 0f), 10 * Time.deltaTime);
     }
 }
